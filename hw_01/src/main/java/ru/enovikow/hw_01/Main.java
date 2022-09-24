688db258ded704f173fe07dd2b801b87df634026
Elisey <enovikow6@gmail.com>
2022-09-24 16:35:28 +0300

    init commit

---
+++ hw_01/src/main/java/ru/enovikow/hw_01/Main.java
@@ -0,0 +1,17 @@
+package ru.enovikow.hw_01;
+
+public class Main {
+    public static void main(String[] args) {
+//        String filePath = args[0];
+//
+//        if (nonNull(filePath)) {
+//            JsonFileParser jsonFileParser = new JsonFileParserImpl();
+//            FileReaderProcessor stringProcessor = new StringFileReaderProcessor(jsonFileParser);
+//            FileReaderProcessor enumProcessor = new EnumFileReaderProcessor(jsonFileParser);
+//            FileReaderService service = new FileReaderServiceImpl(stringProcessor, enumProcessor);
+//
+//            service.readFile(filePath, ReaderType.STRING);
+//            service.readFile(filePath, ReaderType.ENUM);
+//        }
+    }
+}
